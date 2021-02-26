da960ffd9b480bdcde5eee939af43c13d74c769e
Insensual <agamble026@gmail.com>
2021-02-26 00:07:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+# let a user enter which of these three values
+userPlay = input('Enter a play: ')
+# you also need to randomly select an option for the computer/opponent to play.
+compPlayGen = random.randrange(1,4)
+  # maybe store opponents plays in a dicitionary
+compPlay = {
+  'Rock' : 1,
+  'Paper' : 2,
+  'Scissors' : 3,
+}
+# Display both of these selections
+if compPlayGen = 1:
+  compPlayGen = compPlay('Rock')
+# display a message that decides if the user won or lost (or if there was a tie!)
